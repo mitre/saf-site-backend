a8ad155d0bc82e417848fd2970efc940eaa09b4c
Gavin Mason <75515923+GavMason@users.noreply.github.com>
2022-07-25 13:55:35 -0400

    Fixed database.js config file

--- config/database.js
+++ config/database.js
@@ -1,17 +1,38 @@
-const parse = require('pg-connection-string').parse;
-const config = parse("postgres://postgres@127.0.0.1:5432/strapi");
+const { parse } = require("pg-connection-string");
 
-module.exports = ({ env }) => ({
-  connection: {
-    client: 'postgres',
+module.exports = ({ env }) => {
+  const { host, port, database, user, password } = parse(env("DATABASE_URL"));
+
+  return {
     connection: {
-      host: config.host,
-      port: config.port,
-      database: config.database,
-      user: config.user,
-      password: config.password,
-      ssl: false
+      client: "postgres",
+      connection: {
+        host,
+        port,
+        database,
+        user,
+        password,
+        schema: env("DATABASE_SCHEMA", "public"), // Not required
+        ssl: {
+          rejectUnauthorized: env.bool("DATABASE_SSL_SELF", false), // For self-signed certificates
+        },
+      },
+      debug: false,
     },
-    debug: false,
-  },
-});
+  };
+};
+
+// Use this configuration for an SQLite database on your machine.
+// module.exports = ({ env }) => ({
+//   connection: {
+//     client: "sqlite",
+//     connection: {
+//       filename: path.join(
+//         __dirname,
+//         "..",
+//         env("DATABASE_FILENAME", ".tmp/data.db")
+//       ),
+//     },
+//     useNullAsDefault: true,
+//   },
+// });
