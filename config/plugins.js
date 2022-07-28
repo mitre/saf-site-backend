fedc7321abc61372c1b0c1503fc891c0fd3e75c3
Gavin Mason <75515923+GavMason@users.noreply.github.com>
2022-07-28 12:40:24 -0400

    Updated graphql to have introspection

--- config/plugins.js
+++ config/plugins.js
@@ -13,4 +13,17 @@ module.exports = ({ env }) => ({
       },
     },
   },
+  graphql: {
+    config: {
+        endpoint: '/graphql',
+        shadowCRUD: true,
+        playgroundAlways: true,
+        depthLimit: 7,
+        amountLimit: 100,
+        apolloServer: {
+            tracing: false,
+            introspection: true
+        }
+    }
+  },
 });
