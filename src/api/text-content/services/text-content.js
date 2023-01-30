b81a343a66b257bd133e96b27c7247f534edd1fa
Will Dower <wdower@mitre.org>
2023-01-30 10:32:43 -0500

    added general-purpose text-content field, added community enum to partners
    Signed-off-by: Will Dower <wdower@mitre.org>

---
+++ src/api/text-content/services/text-content.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * text-content service
+ */
+
+const { createCoreService } = require('@strapi/strapi').factories;
+
+module.exports = createCoreService('api::text-content.text-content');
