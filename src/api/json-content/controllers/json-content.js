d87e32fa97ae58cbb9ad06cfb5de9959c76855ad
Christopher Andrew Hinchey <chinchey@mitre.org>
2023-02-20 14:36:50 -0500

    Added json dynamic content collection type
    Signed-off-by: Christopher Andrew Hinchey <chinchey@mitre.org>

---
+++ src/api/json-content/controllers/json-content.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * json-content controller
+ */
+
+const { createCoreController } = require('@strapi/strapi').factories;
+
+module.exports = createCoreController('api::json-content.json-content');
---
+++ src/api/json-content/routes/json-content.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * json-content router
+ */
+
+const { createCoreRouter } = require('@strapi/strapi').factories;
+
+module.exports = createCoreRouter('api::json-content.json-content');
