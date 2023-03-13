d2e83966ea1667bc6f4ce2b2c1a4dce573fdc9b0
Christopher Andrew Hinchey <chinchey@mitre.org>
2023-03-13 14:18:35 -0400

    Init for framework pages content
    Signed-off-by: Christopher Andrew Hinchey <chinchey@mitre.org>

---
+++ src/api/framework-page/controllers/framework-page.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * framework-page controller
+ */
+
+const { createCoreController } = require('@strapi/strapi').factories;
+
+module.exports = createCoreController('api::framework-page.framework-page');
---
+++ src/api/framework-page/routes/framework-page.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * framework-page router
+ */
+
+const { createCoreRouter } = require('@strapi/strapi').factories;
+
+module.exports = createCoreRouter('api::framework-page.framework-page');
