1fcc24f58eb19a1b999a49bef69ab39e65bf1df8
Gavin Mason <75515923+GavMason@users.noreply.github.com>
2023-03-21 13:54:25 -0400

    Added capability schema
    Signed-off-by: Gavin Mason <75515923+GavMason@users.noreply.github.com>

---
+++ src/api/capability/controllers/capability.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * capability controller
+ */
+
+const { createCoreController } = require('@strapi/strapi').factories;
+
+module.exports = createCoreController('api::capability.capability');
