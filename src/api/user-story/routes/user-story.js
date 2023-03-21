450eba40c6581f39d0e4703becab6621f2be05b8
Gavin Mason <75515923+GavMason@users.noreply.github.com>
2023-03-21 13:58:12 -0400

    Added user story schema
    Signed-off-by: Gavin Mason <75515923+GavMason@users.noreply.github.com>

---
+++ src/api/user-story/routes/user-story.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * user-story router
+ */
+
+const { createCoreRouter } = require('@strapi/strapi').factories;
+
+module.exports = createCoreRouter('api::user-story.user-story');
