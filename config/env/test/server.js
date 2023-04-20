1842060808d9c6a54c6fa7cb36810c68d504c480
Gavin Mason <75515923+GavMason@users.noreply.github.com>
2023-04-20 14:11:38 -0400

    trival commit
    Signed-off-by: Gavin Mason <75515923+GavMason@users.noreply.github.com>

--- config/env/test/server.js
+++ config/env/test/server.js
@@ -1,10 +1,11 @@
-// module.exports = ({ env }) => ({
-//   host: env("HOST", "0.0.0.0"),
-//   port: env("PORT", 1337),
-//   app: {
-//     keys: env.array("APP_KEYS", ["testKey1", "testKey2"]),
-//   },
-//   webhooks: {
-//     populateRelations: false,
-//   },
-// });
+
+module.exports = ({ env }) => ({
+  host: env("HOST", "0.0.0.0"),
+  port: env("PORT", 1337),
+  app: {
+    keys: env.array("APP_KEYS", ["testKey1", "testKey2"]),
+  },
+  webhooks: {
+    populateRelations: false,
+  },
+});
