6ced23a987d878309d3476e3b13c72b3c5deafb9
Christopher Andrew Hinchey <chinchey@mitre.org>
2023-05-19 17:17:54 -0400

    Fix credential change bug
    Signed-off-by: Christopher Andrew Hinchey <chinchey@mitre.org>

--- config/database.js
+++ config/database.js
@@ -1,13 +1,19 @@
+const { parse } = require("pg-connection-string");
+
 module.exports = ({ env }) => {
+  let parsedUrl = undefined
+  if (env("DATABASE_URL") !== undefined) {
+    parsedUrl = parse(env("DATABASE_URL"));
+  }
   return {
     connection: {
       client: "postgres",
       connection: {
-        host: env("DATABASE_HOST"),
-        port: env("DATABASE_PORT"),
-        database: env("DATABASE_NAME"),
-        user: env("DATABASE_USERNAME"),
-        password: env("DATABASE_PASSWORD"),
+        host: parsedUrl ? parsedUrl.host : env("DATABASE_HOST"),
+        port: parsedUrl ? parsedUrl.port : env("DATABASE_PORT"),
+        database: parsedUrl ? parsedUrl.database : env("DATABASE_NAME"),
+        user: parsedUrl ? parsedUrl.user : env("DATABASE_USERNAME"),
+        password: parsedUrl ? parsedUrl.password : env("DATABASE_PASSWORD"),
         schema: env("DATABASE_SCHEMA", "public"), // Not required
         ssl: {
           rejectUnauthorized: env.bool("DATABASE_SSL_SELF", false), // For self-signed certificates
