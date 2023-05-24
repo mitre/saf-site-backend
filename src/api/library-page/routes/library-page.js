7e34c1c16a41d050f14007c45ae7623e455feabd
Christopher Andrew Hinchey <chinchey@mitre.org>
2023-05-24 17:39:39 -0400

    Init for CKeditor upgrade and added more types
    Signed-off-by: Christopher Andrew Hinchey <chinchey@mitre.org>

---
+++ src/api/library-page/routes/library-page.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * library-page router
+ */
+
+const { createCoreRouter } = require('@strapi/strapi').factories;
+
+module.exports = createCoreRouter('api::library-page.library-page');
